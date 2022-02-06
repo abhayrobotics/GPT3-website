ebd6a83a4265bca39a8f5cd68e4f02e2f544c6c4
abhaygupt <abhayrobotics1908@gmail.com>
2022-02-06 14:26:07 +0530

    Folder structure created and imported to App.js

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css'
+
+const Header = () => {
+  return (
+      <div>
+         HEader
+      </div>
+  );
+};
+
+export default Header;
---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './possibility.css'
+
+const Possibilty = () => {
+  return (
+      <div>
+          Possibilty
+      </div>
+  );
+};
+
+export default Possibilty;
