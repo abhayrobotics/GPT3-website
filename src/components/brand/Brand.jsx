ceb5396e160c327d3587dbf8743fcd17400dea31
abhaygupt <abhayrobotics1908@gmail.com>
2022-02-13 00:42:04 +0530

    Possibility section complete

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './brand.css'
 
+import google from '../../assets/google.png'
+import dropbox from '../../assets/dropbox.png'
+import shopify from '../../assets/shopify.png'
+import slack from '../../assets/slack.png'
+import atlassian from '../../assets/atlassian.png'
+
 const Brand = () => {
   return (
       <div>
-          Brand
+        <div className="brand__logo">
+          <img src={google} alt="google" />
+          <img src={slack} alt="slack" />
+          <img src={atlassian} alt="atlassian" />
+          <img src={shopify} alt="shopify" />
+          <img src={dropbox} alt="dropbox" />
+        </div>
       </div>
   );
 };
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -4,7 +4,66 @@ import './possibility.css'
 const Possibilty = () => {
   return (
       <div>
-          Possibilty
+          <div className="possibility">
+
+            <div className="line"></div>
+            <div className="wgpt3">
+              <div className="wgpt3__title">
+                <h2>What is GPT-3</h2>
+              </div>
+              <div className="wgpt3__desc">
+                <p>We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.</p>
+              </div>
+            </div>
+
+            <div className="possibility__heading ">
+              <h2 className='gradient__text--small'>The possibilities are beyond <br /> 
+              your imagination</h2>
+              <p className='gradient__text--small'>Explore The Library</p>
+            </div>
+
+            <div className="gpt3__content">
+
+              <div className="gpt3__card">
+                <div className="card__title">
+                  <div className="line"></div>
+                  <h2>Chats</h2>
+                </div>
+                <div className="card__desc">
+                  <p>
+                  We so opinion friends me message as delight. Whole front do of plate heard oh ought. 
+                  </p>
+                </div>
+              </div>
+
+              <div className="gpt3__card">
+                <div className="card__title">
+                  <div className="line"></div>
+                  <h2>Knowledgebase</h2>
+                </div>
+                <div className="card__desc">
+                  <p>
+                  We so opinion friends me message as delight. Whole front do of plate heard oh ought. 
+                  </p>
+                </div>
+              </div>
+
+              <div className="gpt3__card">
+                <div className="card__title">
+                  <div className="line"></div>
+                  <h2>Education</h2>
+                </div>
+                <div className="card__desc">
+                  <p>
+                  At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b
+                  </p>
+                </div>
+              </div>
+
+
+            </div>
+
+          </div>
       </div>
   );
 };
