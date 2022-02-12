e227cd639790fda609bf39aef49b40bea6a3bc96
abhaygupt <abhayrobotics1908@gmail.com>
2022-02-13 02:45:22 +0530

    CTA complte

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -4,7 +4,14 @@ import './cta.css'
 const CTA = () => {
   return (
       <div>
-          CTA
+         <div className="cta">
+           <div className="cta__text">
+        
+              <p>Request Early Access to Get Started</p>
+              <h3>Register today & start exploring the endless possiblities</h3>
+           </div>
+           <div className="cta__button">Get started</div>
+         </div>
       </div>
   );
 };
